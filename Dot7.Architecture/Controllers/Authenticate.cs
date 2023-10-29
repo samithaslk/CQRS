6a611215a7b5269ea2fc8a0beabbd8fb1f62330e
Samitha Senarath <Samitha.Senarath@connx.com.au>
2023-10-29 19:08:32 +0530

    added controller authentication

---
+++ Dot7.Architecture/Controllers/Authenticate.cs
@@ -0,0 +1,22 @@
+﻿using Dot7.Architecture.Application.Authenticate;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dot7.Architecture.Api.Controllers
+{
+    public class Authenticate : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public Authenticate(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> AuthenticateGuest([FromBody] GetAuthenticateRequest request)
+        {
+            var response = await _mediator.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
