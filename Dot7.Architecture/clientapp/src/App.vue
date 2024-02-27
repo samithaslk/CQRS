7eaf7184a00d92461e7f69066f139c0396fb5fe1
Samitha Senarath <samitha.senarath@connx.com.au>
2024-02-27 08:09:08 +0530

    added VUE.JS

---
+++ Dot7.Architecture/clientapp/src/App.vue
@@ -0,0 +1,44 @@
+<template> 
+    <div id="nav">
+        <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Logo Here</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-nav mr-auto">
+                    <router-link class="nav-item nav-link" to="/">Home</router-link>
+                    <router-link class="nav-item nav-link" to="fetchBooks" id="view" :key='$route.params.id || $route.params'>Books</router-link>
+                </div>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </div>
+    <router-view />
+</template>
+
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'; 
+
+@Options({
+  
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
